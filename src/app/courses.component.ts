a7e0fdc2d31630a992e075371bd2a4f6744d68d6
Leslie Calimlim <Leslie.Calimlim@fdmgroup.com>
2019-08-22 17:16:00 -0400

    Completed up to pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,22 +3,44 @@ import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
-    template: `
-     <h2>{{ title }}</h2>
-     <ul>
-        <li *ngFor="let course of courses">
-            {{ course }}
-        </li>
-     </ul>
-    `
+    templateUrl: './courses.component.html'
 })
 export class CoursesComponent{
     title = "List of courses";
     courses;
+    imageUrl = "";
+    colSpan = 2;
+    isActive = true;
+    email ="me@example.com";
+
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    }
+
+    text = `jfhdjf asdfh ashf adfla lfkjasdh`;
 
     //Dependency injection
     constructor(service: CoursesService){
         this.courses = service.getCourses();
     }
+
+    onSave($event){
+        $event.stopPropagation();
+        console.log("Button was clicked", $event);
+    }
+
+    onDivClicked(){
+        console.log("Div was clicked");
+    }
+
+    onKeyUp(){
+        console.log(this.email);
+    }
+
+    
 
 }
